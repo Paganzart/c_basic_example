a7cc7cb6c84f8a25e15abd07381a60b92e51bbbc
Alex <redencionzodiaco@hotmail.com>
2021-11-25 13:39:54 -0600

    mi segundo commit

---
+++ operaciones.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, suma;
+
+    printf("Ingrese a: ");
+    scanf("%d", &a);
+    printf("Ingrese b: ");
+    scanf("%d", &b);
+
+    suma = a + b;
+    printf("El resultado es: %d", suma);
+
+    return 0;
+}
